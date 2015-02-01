57fec1d200e2ecc632a0b37911bf18f3450476ef
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-02-01 07:00:00 -0400

    add amd script example

--- example/amd/script.js
+++ example/amd/script.js
@@ -8,10 +8,34 @@ require.config({
 
 define(['jquery', 'notifyr'], function ($, notifyr) {
 
-  function slowAlert() {
-    $('#target').notifyr();
+  $('#trigger-notification-1').on('click', function(e) {
+    e.preventDefault();
+    var data = $(e.target).data();
+    $('#notifications').notifyr({message: data.notificationMessage});
+  });
+
+  $('#trigger-notification-2').on('click', function(e) {
+    e.preventDefault();
+    var data = $(e.target).data();
+    $('#notifications').notifyr({message: data.notificationMessage, location: data.notificationLocation});
+  });
+
+  $('#trigger-notification-3').on('click', function(e) {
+    e.preventDefault();
+    var data = $(e.target).data();
+    $('#notifications').notifyr({message: data.notificationMessage, location: data.notificationLocation, title: 'A title here'});
+  });
+
+  $('#trigger-notification-4').on('click', function(e) {
+    e.preventDefault();
+    var data = $(e.target).data();
+    $('#notifications').notifyr({message: data.notificationMessage, location: data.notificationLocation});
+  });
+
+  function delayedNotice() {
+    $('#notifications').notifyr({title: 'Delayed notification', message: 'This is the message of the notification'});
   }
 
-  window.setTimeout(slowAlert, 2000);
+  window.setTimeout(delayedNotice, 2000);
 
 });
