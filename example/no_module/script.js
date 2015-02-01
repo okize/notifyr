d35504aeec93339dd76a94b7a3335b6a6455d304
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-02-01 07:00:00 -0400

    add script example for no modules

--- example/no_module/script.js
+++ example/no_module/script.js
@@ -2,10 +2,34 @@ $(document).on('ready', function() {
 
   'use strict';
 
-  function slowAlert() {
-    $('#target').notifyr();
+  $('#trigger-notification-1').on('click', function(e) {
+    e.preventDefault();
+    var data = $(e.target).data();
+    $('#notifications').notifyr({message: data.notificationMessage});
+  });
+
+  $('#trigger-notification-2').on('click', function(e) {
+    e.preventDefault();
+    var data = $(e.target).data();
+    $('#notifications').notifyr({message: data.notificationMessage, location: data.notificationLocation});
+  });
+
+  $('#trigger-notification-3').on('click', function(e) {
+    e.preventDefault();
+    var data = $(e.target).data();
+    $('#notifications').notifyr({message: data.notificationMessage, location: data.notificationLocation, title: 'A title here'});
+  });
+
+  $('#trigger-notification-4').on('click', function(e) {
+    e.preventDefault();
+    var data = $(e.target).data();
+    $('#notifications').notifyr({message: data.notificationMessage, location: data.notificationLocation});
+  });
+
+  function delayedNotice() {
+    $('#notifications').notifyr({title: 'Delayed notification', message: 'This is the message of the notification'});
   }
 
-  window.setTimeout(slowAlert, 2000);
+  window.setTimeout(delayedNotice, 2000);
 
 });
